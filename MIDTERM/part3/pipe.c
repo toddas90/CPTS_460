2b190e2d22d02789f1422f8c414b1a65546b141b
Andrew Todd <andrew123ast@gmail.com>
2022-02-24 09:56:04 -0800

    Added pipe code from the book. Implemented pipe_init, create_pipe, read_pipe, and write_pipe.

--- MIDTERM/part3/pipe.c
+++ MIDTERM/part3/pipe.c
@@ -6,7 +6,7 @@ typedef struct pipe{
   char buf[PSIZE];
   int head, tail, data, room;
   int status;
-  // int nreader, nwriter;    // number of reader, writer on this pipe
+  int nreader, nwriter;
 }PIPE;
 
 
@@ -15,23 +15,105 @@ PIPE pipe[NPIPE];
 
 int pipe_init()
 {
-  // fill in code
+    for (int i = 0; i < NPIPE; i++) {
+        for (int j = 0; j < PSIZE; j++) {
+            pipe[i].buf[j] = 0;
+        }
+        pipe[i].head = 0;
+        pipe[i].tail = 0;
+        pipe[i].data = 0;
+        pipe[i].room = PSIZE;
+        pipe[i].nreader = 0;
+        pipe[i].nwriter = 0;
+        pipe[i].status = FREE;
+    }
 }
 
 PIPE *create_pipe()
 {
-  // fill in code
+    PIPE *new_pipe;
+    for (int i = 0; i < NPIPE; i++) {
+        if (pipe[i].status == FREE) {
+            // Free pipe found
+            for (int j = 0; j < PSIZE; j++) {
+                pipe[i].buf[j] = 0;
+            }
+            pipe[i].head = 0;
+            pipe[i].tail = 0;
+            pipe[i].data = 0;
+            pipe[i].room = PSIZE;
+            pipe[i].nreader = 1;
+            pipe[i].nwriter = 1;
+            pipe[i].status = READY;
+        }
+    }
+    printf("No free pipes!\n");
+    return 0;
 }
 
 int read_pipe(PIPE *p, char *buf, int n)
 {
-  // fill in code;
+    int r = 0;
+
+    if (n <= 0) {
+        return 0;
+    }
+
+    if (p->status == FREE) {
+        return -1;
+    }
+
+    while (n) {
+        while (p->data) {
+            *buf++ = p->buf[p->tail++]; // Read byte into buf
+            p->tail %= PSIZE;
+            p->data--; p->room++; r++; n--;
+
+            if (n == 0) {
+                break;
+            }
+        }
+        wakeup(&p->room); // Wake up writers
+        
+        if (r) {
+            return r;
+            // Pipe has no data
+            sleep(&p->data); // Sleep for data
+        }
+    }
   // add code to handle: no data AND no writer: return 0
 }
 
 int write_pipe(PIPE *p, char *buf, int n)
 {
-  // fill in code;
+    int r = 0;
+
+    if (n <= 0) {
+        return 0;
+    }
+
+    if (p->status == FREE) {
+        return -1;
+    }
+
+    while (n) {
+        while (p->room) {
+            p->buf[p->head++] = *buf++; // Write a byte to pipe
+            p->head %= PSIZE;
+            p->data++; p->room--; r++; n--;
+
+            if (n == 0) {
+                break;
+            }
+            wakeup(&p->data); // Wake up readers
+
+            if (n == 0) {
+                return r; // Finished writing n bytes
+            }
+            // More data, no more room
+            sleep(&p->room); // Sleep for room
+        }
+    }
   // add code to detect BROKEN pipe: print BROKEN pipe message, then exit
 }
 
