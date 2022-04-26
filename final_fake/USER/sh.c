f17a0513977d2027a3b8216503918f893b9967a5
Andrew Todd <andrew123ast@gmail.com>
2022-04-25 20:38:14 -0700

    Pipes aren't working.

--- final_fake/USER/sh.c
+++ final_fake/USER/sh.c
@@ -2,6 +2,37 @@
 
 #include "ucode.c"
 
+int do_pipe(char *cmdline) {
+    char *cmd1, *cmd2;
+    int pd[2];
+    int pid;
+
+    cmd1 = strtok(cmdline, "|");
+    cmd2 = strtok(NULL, "|");
+
+    if (cmd1 == NULL) {
+        return 0;
+    }
+
+    if (cmd2 == NULL) {
+        exec(cmd1);
+        return 0;
+    }
+
+    pipe(pd);
+    pid = fork();
+
+    if (pid == 0) {
+        close(pd[0]);
+        dup2(pd[1], 1);
+        do_pipe(cmd2);
+    } else {
+        close(pd[1]);
+        dup2(pd[0], 0);
+        do_pipe(cmd1);
+    }
+}
+
 int main(int argc, char *argv[ ])
 {
     // pid and status.
@@ -24,8 +55,15 @@ int main(int argc, char *argv[ ])
         if (cmd == NULL) {
             continue;
         }
-        
-        if (strcmp(cmd, "exit") == 0) { // Trivial case: exit.
+
+        // Checks for pipe in the command line.
+        if (strstr(cmdline, "|")) {
+            do_pipe(cmdline);
+            continue;
+        }
+
+        // Trivial case exit.
+        if (strcmp(cmd, "exit") == 0) {
             printf("Exiting shell\n");
             exit(0);
         }
