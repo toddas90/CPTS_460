e2355630cf12ebfff673bc3edf1fe59dac474bc0
Andrew Todd <andrew123ast@gmail.com>
2022-04-26 22:10:28 -0700

    Init works when forking 1 tty, but loops when trying to fork more than 1.

--- final_fake/USER/init.c
+++ final_fake/USER/init.c
@@ -2,38 +2,73 @@
 
 #include "ucode.c"
 
-int console;
+int console[3];
+char ttys[3][16] = {"/dev/tty0", "/dev/ttyS0", "/dev/ttyS1"};
 
-int parent() {
+void main() {
+    int i;
+    int in[3], out[3];
     int pid, status;
 
+    for (i = 0; i < 3; i++) {
+        in[i] = open(ttys[i], O_RDONLY);
+        out[i] = open(ttys[i], O_WRONLY);
+        console[i] = fork();
+        if (console[i] == 0) {
+            exec(strcat("login ", ttys[i]));
+        }
+    }
+    
     while (1) {
         pid = wait(&status);
         
-        if (pid == console) {
-            printf("Forking new console process\n");
-            console = fork();
-            if (console) {
-                continue;
-            } else {
-                exec("login /dev/tty0");
+        for (int i = 0; i < 3; i++) {
+            if (pid == console[i]) {
+                printf("Forking new console process\n");
+                console[i] = fork();
+                if (console[i]) {
+                    continue;
+                } else {
+                    exec(strcat("login ", ttys[i]));
+                }
+                printf("Buried process %d\n", pid);
             }
-            printf("Buried process %d\n", pid);
         }
     }
 }
 
-void main() {
-    int in, out;
-    in = open("/dev/tty0", O_RDONLY);
-    out = open("/dev/tty0", O_WRONLY);
-    printf("Forking login process\n");
+// int console;
 
-    console = fork();
+// int parent() {
+//     int pid, status;
 
-    if (console) {
-        parent();
-    } else {
-        exec("login /dev/tty0");
-    }
-}
+//     while (1) {
+//         pid = wait(&status);
+        
+//         if (pid == console) {
+//             printf("Forking new console process\n");
+//             console = fork();
+//             if (console) {
+//                 continue;
+//             } else {
+//                 exec("login /dev/tty0");
+//             }
+//             printf("Buried process %d\n", pid);
+//         }
+//     }
+// }
+
+// void main() {
+//     int in, out;
+//     in = open("/dev/tty0", O_RDONLY);
+//     out = open("/dev/tty0", O_WRONLY);
+//     printf("Forking login process\n");
+
+//     console = fork();
+
+//     if (console) {
+//         parent();
+//     } else {
+//         exec("login /dev/tty0");
+//     }
+// }
